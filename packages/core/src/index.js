be27e24427678278f56ffba995b25f1b3a655971
ryantoddgarza <ryantoddgarza@gmail.com>
2021-03-26 12:28:31 -0500

    fix(core): add es required file extension

--- packages/core/src/index.js
+++ packages/core/src/index.js
@@ -1,3 +1,3 @@
-import '../../../lib/AudioContextMonkeyPatch/AudioContextMonkeyPatch';
+import '../../../lib/AudioContextMonkeyPatch/AudioContextMonkeyPatch.js';
 
 export { default as InterfaceParam } from './InterfaceParam';
